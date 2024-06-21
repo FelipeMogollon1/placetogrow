7819aa5c6683a0fb340ac26084e1437d74a72ddb
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-06-20 23:17:03 -0500

    feat: Added migrations for tokens, categories and microsites tables

---
+++ database/migrations/2024_06_20_152241_create_microsites_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Constants\MicrositesTypes;
+use App\Constants\DocumentTypes;
+use App\Constants\CurrencyTypes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('microsites', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 150)->unique();
+            $table->string('name', 150);
+            $table->string('logo')->nullable();
+            $table->enum('document_type', DocumentTypes::getDocumentTypes())->nullable();
+            $table->string('document', 20)->nullable();
+            $table->enum('microsite_type', MicrositesTypes::getMicrositesTypes());
+            $table->enum('currency', CurrencyTypes::getCurrencyType());
+            $table->integer('payment_expiration_time');
+            $table->foreignId('category_id')->constrained();
+            $table->timestamp('enabled_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('microsites');
+    }
+};
