3552cbdcf0ca0b7b7c3e321fe24e5ebf26156dc7
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-06-28 22:25:35 -0500

    feat: add factory model for categories

--- app/Infrastructure/Persistence/Models/Category.php
+++ app/Infrastructure/Persistence/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\Persistence\Models;
 
+use Database\Factories\CategoryFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -26,6 +27,10 @@ class Category extends Model
         return $this->hasMany(Microsite::class);
     }
 
+    protected static function newFactory()
+    {
+        Return CategoryFactory::new();
+    }
 
 
 }
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Infrastructure\Persistence\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class CategoryFactory extends Factory
 {
+    protected $model = Category::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,9 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name'=> $this->faker->name(),
+            'description'=> $this->faker->name(),
+            'enabled_at'=> now()
         ];
     }
 
