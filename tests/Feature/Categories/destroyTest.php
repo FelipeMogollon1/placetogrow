6fbd5788d9d4aef86ae19f543a1e77ddc91d6412
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-06-30 01:07:47 -0500

    feat: add test for categories

---
+++ tests/Feature/Categories/destroyTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Categories;
+
+use App\Infrastructure\Persistence\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class destroyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_can_destroy_categories(): void
+    {
+        $user = User::factory()->create();
+
+        $category = Category::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->delete(route('categories.destroy', $category->id));
+
+        $response->assertRedirect(route('categories.index'));
+
+    }
+}
