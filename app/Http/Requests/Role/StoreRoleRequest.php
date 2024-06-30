a55ef6a6818cedb3884fb374dcfad3f0e6bafe68
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-06-30 01:59:16 -0500

    feat: add validation for input permissions

--- app/Http/Requests/Role/StoreRoleRequest.php
+++ app/Http/Requests/Role/StoreRoleRequest.php
@@ -15,8 +15,8 @@ class StoreRoleRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255|unique:roles,name',
-            //'permissions' => 'required|array',
-         //   'permissions.*' => 'exists:permissions,name',
+            'permissions' => 'required|array',
+           'permissions.*' => 'exists:permissions,name',
         ];
     }
 }
