9a61e0b302f45e70bbcc6d6c586324f9d87f6759
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-07-19 01:33:49 -0500

    feat: add relationship with model users

---
+++ app/Http/Controllers/Admin/MicrositeController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Constants\Abilities;
+use App\Constants\CurrencyTypes;
+use App\Constants\DocumentTypes;
+use App\Constants\MicrositesTypes;
+use App\Domain\Microsite\Actions\DestroyMicrositeAction;
+use App\Domain\Microsite\Actions\StoreMicrositeAction;
+use App\Domain\Microsite\Actions\UpdateMicrositeAction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Microsite\StoreMicrositeRequest;
+use App\Http\Requests\Microsite\UpdateMicrositeRequest;
+use App\Infrastructure\Persistence\Models\Category;
+use App\Infrastructure\Persistence\Models\Microsite;
+use App\Infrastructure\Persistence\Models\User;
+use App\ViewModels\Microsites\MicrositeIndexViewModel;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class MicrositeController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function index(): Response
+    {
+        /** @var User $user */
+        $user = Auth::user();
+        $this->authorize(Abilities::VIEW_ANY->value, Microsite::class);
+
+        return Inertia::render('Microsites/Index', new MicrositeIndexViewModel($user));
+    }
+
+    public function create(): Response
+    {
+        $this->authorize(Abilities::CREATE->value   , Microsite::class);
+        $arrayConstants = $this->getCommonData();
+
+        return Inertia::render('Microsites/Create', $arrayConstants);
+    }
+
+    public function store(StoreMicrositeRequest $request, StoreMicrositeAction $storeAction): RedirectResponse
+    {
+        $this->authorize(Abilities::STORE->value, Microsite::class);
+        $storeAction->execute($request->validated());
+
+        return to_route('microsites.index');
+    }
+
+    public function show(string $id): Response
+    {
+        $this->authorize(Abilities::VIEW->value, Microsite::class);
+        $microsite = Microsite::withCategory($id)->firstOrFail()->toArray();
+
+        return Inertia::render('Microsites/Show', compact('microsite'));
+    }
+
+    public function edit(string $id): Response
+    {
+        $this->authorize(Abilities::EDIT->value, Microsite::class);
+        $microsite = Microsite::findOrFail($id);
+        $arrayConstants = $this->getCommonData();
+
+        return Inertia::render('Microsites/Edit', compact('microsite', 'arrayConstants'));
+    }
+
+    public function update(UpdateMicrositeRequest $request, string $id, UpdateMicrositeAction $updateAction): RedirectResponse
+    {
+        $this->authorize(Abilities::UPDATE->value, Microsite::class);
+        $updateAction->execute($id, $request->validated());
+
+        return to_route('microsites.index');
+    }
+
+    public function destroy(int $id,DestroyMicrositeAction $destroyAction): RedirectResponse
+    {
+        $this->authorize(Abilities::DELETE->value, Microsite::class);
+        $destroyAction->execute($id);
+
+        return to_route('microsites.index');
+    }
+
+    public function welcomeIndex(): Response
+    {
+        $microsites = Microsite::AllWithCategories()->get();
+
+        return Inertia::render('Welcome', compact('microsites'));
+
+    }
+    public function paymentForm(): Response
+    {
+        return Inertia::render('PaymentForm');
+    }
+
+    private function getCommonData(): array
+    {
+        return [
+            'documentTypes' => DocumentTypes::getDocumentTypes(),
+            'micrositesTypes' => MicrositesTypes::getMicrositesTypes(),
+            'currencyTypes' => CurrencyTypes::getCurrencyType(),
+            'categories' => Category::select('id', 'name')->get(),
+            'users' => User::role('admin')->select('id', 'name')->get(),
+        ];
+    }
+}
--- app/Infrastructure/Persistence/Models/Microsite.php
+++ app/Infrastructure/Persistence/Models/Microsite.php
@@ -3,6 +3,8 @@
 namespace App\Infrastructure\Persistence\Models;
 
 use Database\Factories\MicrositeFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -25,16 +27,42 @@ class Microsite extends Model
         'currency',
         'payment_expiration_time',
         'category_id',
+        'user_id',
         'enabled_at',
     ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    protected static function newFactory()
+    public function scopeWithCategory(Builder $query, $id = null): Builder
+    {
+        return $query->with(['category', 'user'])->where('id', $id);
+
+    }
+
+    public function scopeAllWithCategories(Builder $query): Builder
+    {
+        return $query
+            ->leftJoin('categories', 'microsites.category_id', '=', 'categories.id')
+            ->leftJoin('users', 'microsites.user_id', '=', 'users.id')
+            ->select([
+                'microsites.id',
+                'microsites.name',
+                'microsites.microsite_type',
+                'microsites.logo',
+                'categories.name as category_name',
+                'users.name as user_name',
+            ]);
+    }
+
+    protected static function newFactory(): Factory|MicrositeFactory
     {
         return MicrositeFactory::new();
     }
