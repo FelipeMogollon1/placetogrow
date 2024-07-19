31d60a69d031c00e6a8bc4a24a2f74ec1a493719
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-07-18 22:54:02 -0500

    feat: add relations belongs to table user

--- app/Infrastructure/Persistence/Models/Microsite.php
+++ app/Infrastructure/Persistence/Models/Microsite.php
@@ -3,6 +3,7 @@
 namespace App\Infrastructure\Persistence\Models;
 
 use Database\Factories\MicrositeFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -26,28 +27,36 @@ class Microsite extends Model
         'currency',
         'payment_expiration_time',
         'category_id',
+        'user_id',
         'enabled_at',
     ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function scopeWithCategory($query, $id = null)
+    public function scopeWithCategory(Builder $query, $id = null): Builder
     {
         if ($id !== null) {
-            return $query->with('category')->where('id', $id);
+            return $query->with(['category', 'user'])->where('id', $id);
         }
 
-        return $query->join('categories', 'microsites.category_id', '=', 'categories.id')
+        return $query
+            ->leftJoin('categories', 'microsites.category_id', '=', 'categories.id')
+            ->leftJoin('users', 'microsites.user_id', '=', 'users.id')
             ->select([
                 'microsites.id',
                 'microsites.name',
                 'microsites.microsite_type',
                 'microsites.logo',
-                'categories.name as category_name'
+                'categories.name as category_name',
+                'users.name as user_name',
             ]);
     }
 
@@ -55,6 +64,5 @@ class Microsite extends Model
     {
         return MicrositeFactory::new();
     }
-
 
 }
