4c41dbed5f74a04c22d9af41b32ac6fe21e4353f
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-07-29 22:55:32 -0500

    feat: initial implementation of paging

--- app/ViewModels/Microsites/MicrositeIndexViewModel.php
+++ app/ViewModels/Microsites/MicrositeIndexViewModel.php
@@ -6,7 +6,7 @@ use App\Constants\Permissions;
 use App\Constants\Roles;
 use App\Infrastructure\Persistence\Models\Microsite;
 use App\Infrastructure\Persistence\Models\User;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Spatie\ViewModels\ViewModel;
 
 class MicrositeIndexViewModel extends ViewModel
@@ -16,26 +16,37 @@ class MicrositeIndexViewModel extends ViewModel
 
     }
 
-    public function getMicrositesByUserRole(): Collection
+    public function getMicrositesByUserRole(): LengthAwarePaginator
     {
         $permissionsUser = $this->user->getAllPermissions()->pluck('name')->toArray();
         $rolesUser = $this->user->roles->pluck('name')->toArray();
 
         if (in_array(Permissions::MICROSITES_INDEX->value, $permissionsUser) && in_array(Roles::SA->value, $rolesUser)) {
 
-            $microsites = Microsite::query()->AllWithCategories()->get();
+            $microsites = Microsite::query()->AllWithCategories();
 
         } else {
             $microsites = Microsite::query()->AllWithCategories()
-                ->where('microsites.user_id', $this->user->id)
-                ->get();
+                ->where('microsites.user_id', $this->user->id);
         }
-        return $microsites;
+        return $microsites->paginate(5);
     }
+
     public function toArray(): array
     {
+        $micrositesPaginator = $this->getMicrositesByUserRole();
+
         return [
-            'microsites' => $this->getMicrositesByUserRole(),
+            'microsites' => $micrositesPaginator->items(),
+            'pagination' => [
+                'total' => $micrositesPaginator->total(),
+                'per_page' => $micrositesPaginator->perPage(),
+                'current_page' => $micrositesPaginator->currentPage(),
+                'last_page' => $micrositesPaginator->lastPage(),
+                'from' => $micrositesPaginator->firstItem(),
+                'to' => $micrositesPaginator->lastItem(),
+                'links' => $micrositesPaginator->links(),
+            ],
         ];
     }
 }
