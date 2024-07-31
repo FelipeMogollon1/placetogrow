e0c84598b3df0e85ded46d6e90f6dfa5c10e2aa6
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-07-31 15:12:28 -0500

    feat: we create independent jsons and remove in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
-        $this->call(FormSeeder::class);
         $this->call(MicrositeSeeder::class);
-
     }
 }
