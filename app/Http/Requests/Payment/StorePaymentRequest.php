47a7db41c7b5c75f370e5bf9caa4c820bac9edef
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-08-03 19:24:37 -0500

    feat: update message request for microsites

--- app/Http/Requests/Payment/StorePaymentRequest.php
+++ app/Http/Requests/Payment/StorePaymentRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\Payment;
 use App\Constants\CurrencyTypes;
 use App\Constants\DocumentTypes;
 use App\Constants\PaymentStatus;
+use App\Rules\Payment\UniqueDocumentCombination;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StorePaymentRequest extends FormRequest
@@ -19,19 +20,27 @@ class StorePaymentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'reference' => ['nullable', 'string', 'unique:payments'],
+            'reference' => ['nullable', 'string', 'unique:payments,reference'],
             'receipt' => ['nullable', 'string'],
             'payer_name' => ['nullable', 'string'],
             'payer_surname' => ['nullable', 'string'],
             'payer_email' => ['nullable', 'email'],
-            'payer_phone' => ['nullable', 'numeric'],
+            'payer_phone' => ['nullable', 'numeric', 'min:10'],
             'payer_company' => ['nullable', 'string'],
-            'payer_document_type' => ['nullable', 'string', 'in:' . implode(',', DocumentTypes::getDocumentTypes())],
-            'payer_document' => ['required', 'string', 'unique:payments'],
+            'payer_document_type' => [
+                'nullable',
+                'string',
+                'in:' . implode(',', DocumentTypes::getDocumentTypes())
+            ],
+            'payer_document' => [
+                'nullable',
+                'string',
+                new UniqueDocumentCombination($this->input('payer_document_type'))
+            ],
             'description' => ['nullable', 'string'],
-            'amount' => ['nullable', 'numeric', 'min:0'],
+            'amount' => ['required', 'numeric', 'min:0', 'max:999999999.99'],
             'paid_at' => ['nullable', 'date'],
-            'currency' => ['nullable', 'string', 'in:' . implode(',', CurrencyTypes::getCurrencyType())],
+            'currency' => ['required', 'string', 'in:' . implode(',', CurrencyTypes::getCurrencyType())],
             'status' => ['nullable', 'string', 'in:' . implode(',', PaymentStatus::getPaymentStatus())],
             'process_identifier' => ['nullable', 'integer'],
             'user_id' => ['nullable', 'exists:users,id'],
