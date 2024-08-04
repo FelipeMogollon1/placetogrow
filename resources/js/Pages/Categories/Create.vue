ccd8001355b24d79a734ec59cf664ed13f7e9b10
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-08-04 12:40:16 -0500

    feat: add update view create categories

--- resources/js/Pages/Categories/Create.vue
+++ resources/js/Pages/Categories/Create.vue
@@ -22,17 +22,17 @@ const submit = () => {
 
 <template>
 
-    <Head title="Crear categoria"/>
+    <Head :title="$t('category.create_category')"/>
     <AuthenticatedLayout>
         <template #header>
             <div class="flex justify-between items-center">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">Crear Categoria</h2>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{$t('category.create_category')}}</h2>
                 <Link
                     v-if="can('categories.index')"
                     :href="route('categories.index')"
-                    class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                    class="inline-flex items-center px-4 py-2 bg-orange-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-700 focus:bg-orange-700 active:bg-orange-900 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 transition ease-in-out duration-150"
                 >
-                    Lista de categorias
+                    {{ $t('category.list_category') }}
                 </Link>
             </div>
         </template>
@@ -40,11 +40,11 @@ const submit = () => {
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="flex justify-center p-6 text-gray-900">
-                        <form class="w-1/3 py-8 space-y-5" @submit.prevent="submit">
+                <div class="bg-white overflow-hidden border border-gray-100 sm:rounded-lg shadow-2xl">
+                    <div class="p-6 text-gray-900 ">
+                        <form class="grid grid-cols-1 gap-6 sm:grid-cols-2 " @submit.prevent="submit">
                         <div>
-                            <InputLabel for="name" value="Nombre" />
+                            <InputLabel for="name" :value="$t('category.name')" />
                             <TextInput
                                 id="name"
                                 type="text"
@@ -52,13 +52,13 @@ const submit = () => {
                                 v-model="form.name"
                                 autofocus
                                 autocomplete="name"
-                                placeholder="Tecnología"
+                                :placeholder="$t('category.name')"
                             />
                             <InputError class="mt-2" :message="form.errors.name" />
                         </div>
 
                         <div>
-                            <InputLabel for="description" value="Descripción" />
+                            <InputLabel for="description" :value="$t('category.description')"/>
                             <TextInput
                                 id="name"
                                 type="text"
@@ -66,14 +66,14 @@ const submit = () => {
                                 v-model="form.description"
                                 autofocus
                                 autocomplete="name"
-                                placeholder="Tienda de videojuegos y Computadores"
+                                :placeholder="$t('category.description')"
                             />
                             <InputError class="mt-2" :message="form.errors.description" />
                         </div>
 
-                        <div class="flex justify-center">
+                        <div class="flex justify-center col-span-2">
                             <PrimaryButton >
-                                Crear Categoria
+                                {{$t('category.create_category')}}
                             </PrimaryButton>
                         </div>
                     </form>
