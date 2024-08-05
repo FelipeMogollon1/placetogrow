c4268585fa0af4a8f82bcd0f0634be2ffc68d970
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-08-05 14:43:22 -0500

    refactor: update migration and request for payment

--- app/Http/Requests/Payment/StorePaymentRequest.php
+++ app/Http/Requests/Payment/StorePaymentRequest.php
@@ -7,8 +7,9 @@ use App\Constants\DocumentTypes;
 use App\Constants\PaymentStatus;
 use App\Rules\Payment\DocumentNumber;
 use App\Rules\Payment\UniqueDocumentCombination;
-use Database\Factories\PaymentFactory;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class StorePaymentRequest extends FormRequest
 {
@@ -24,24 +25,45 @@ class StorePaymentRequest extends FormRequest
         return [
             'reference' => ['nullable', 'string', 'unique:payments,reference'],
             'receipt' => ['nullable', 'string'],
-            'payer_name' => ['nullable', 'string'],
-            'payer_surname' => ['nullable', 'string'],
-            'payer_email' => ['nullable', 'email'],
+            'payer_name' => [
+                'nullable',
+                'string',
+                Rule::requiredIf($this->hasAnyPayerFields())
+            ],
+            'payer_surname' => [
+                'nullable',
+                'string',
+                Rule::requiredIf($this->hasAnyPayerFields())
+            ],
+            'payer_email' => [
+                'nullable',
+                'email',
+                Rule::requiredIf($this->hasAnyPayerFields())
+            ],
             'payer_phone' => ['nullable', 'numeric', 'min:10'],
             'payer_company' => ['nullable', 'string'],
             'payer_document_type' => [
                 'nullable',
-                'in:' . implode(',', DocumentTypes::getDocumentTypes())
+                'in:' . implode(',', DocumentTypes::getDocumentTypes()),
+                Rule::requiredIf($this->hasAnyPayerFields())
             ],
             'payer_document' => [
                 'nullable',
                 new DocumentNumber($this->input('payer_document_type')),
-                new UniqueDocumentCombination($this->input('payer_document_type'))
+                new UniqueDocumentCombination($this->input('payer_document_type')),
+                Rule::requiredIf($this->hasAnyPayerFields())
             ],
+
             'description' => ['nullable', 'string'],
-            'amount' => ['required', 'numeric', 'min:0', 'max:999999999.99'],
-            'paid_at' => ['nullable', 'date'],
             'currency' => ['required', 'string', 'in:' . implode(',', CurrencyTypes::getCurrencyType())],
+            'amount' => [
+                'required',
+                'numeric',
+                'min:0',
+                'max:999999999',
+                Rule::requiredIf($this->hasAnyMountCurrency())
+            ],
+            'paid_at' => ['nullable', 'date'],
             'status' => ['nullable', 'string', 'in:' . implode(',', PaymentStatus::getPaymentStatus())],
             'process_identifier' => ['nullable', 'integer'],
             'user_id' => ['nullable', 'exists:users,id'],
@@ -50,8 +72,28 @@ class StorePaymentRequest extends FormRequest
         ];
     }
 
-    protected static function newFactory(): PaymentFactory
+    private function hasAnyPayerFields(): bool
     {
-        return PaymentFactory::new();
+        return $this->input('payer_name') !== null ||
+                $this->input('payer_surname') !== null ||
+                $this->input('payer_email') !== null ||
+                $this->input('payer_document_type') !== null ||
+                $this->input('payer_document') !== null;
+    }
+
+    private function hasAnyMountCurrency(): bool
+    {
+        $currency = $this->input('currency');
+        $amount = $this->input('amount');
+
+        $isValidCurrency = in_array($currency, CurrencyTypes::getCurrencyType());
+        $isAmountValid = floatval($amount) > 99999;
+
+        Log::info('Currency: ' . $currency);
+        Log::info('Amount: ' . $amount);
+        Log::info('Currency valid: ' . ($isValidCurrency ? 'true' : 'false'));
+        Log::info('Amount greater than 99999: ' . ($isAmountValid ? 'true' : 'false'));
+
+        return $isValidCurrency && $isAmountValid;
     }
 }
--- database/migrations/2024_08_02_003328_create_payments_table.php
+++ database/migrations/2024_08_02_003328_create_payments_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->string('reference')->unique()->nullable();
-            $table->string('receipt')->nullable();
+            $table->string('receipt')->unique()->nullable();
             $table->string('payer_name')->nullable();
             $table->string('payer_surname')->nullable();
             $table->string('payer_email')->nullable();
             $table->string('payer_phone')->nullable();
             $table->string('payer_company')->nullable();
             $table->enum('payer_document_type', DocumentTypes::getDocumentTypes())->nullable();
-            $table->string('payer_document')->unique()->nullable();
+            $table->string('payer_document')->nullable();
             $table->string('description')->nullable();
             $table->unsignedBigInteger('amount')->nullable();
             $table->timestamp('paid_at')->nullable();
@@ -37,7 +37,6 @@ return new class extends Migration
             $table->foreign('microsite_id')->references('id')->on('microsites');
             $table->json('additional_data')->nullable();
             $table->timestamps();
-            $table->unique(['payer_document', 'payer_document_type'], 'UNIQUE_PERSONS_PAYMENTS');
         });
     }
 
