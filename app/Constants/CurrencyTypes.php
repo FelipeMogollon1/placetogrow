b9af30813c89e58282f9e20851bcdc277bd3b533
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-09-05 20:13:01 -0500

    feat: create migrate subscriptions plan and constant subscriptionsPeriods

--- app/Constants/CurrencyTypes.php
+++ app/Constants/CurrencyTypes.php
@@ -8,6 +8,13 @@ enum CurrencyTypes: string
     case USD = 'USD';
     case BOTH = 'BOTH';
 
+    public static function getSelectableCurrencies(): array
+    {
+        return [
+            self::COP->value,
+            self::USD->value,
+        ];
+    }
     public static function getCurrencyType(): array
     {
         return array_map(fn($enum) => $enum->value, self::cases());
