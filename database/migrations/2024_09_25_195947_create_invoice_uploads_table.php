8087f7fba8f622bee9137766f841254cdfa0b32f
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-09-25 16:54:33 -0500

    feat: implement migration invoice uploads

---
+++ database/migrations/2024_09_25_195947_create_invoice_uploads_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('invoice_uploads', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('microsite_id')->constrained()->onDelete('cascade');
+            $table->string('storage_path');
+            $table->string('error_file_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_uploads');
+    }
+};
