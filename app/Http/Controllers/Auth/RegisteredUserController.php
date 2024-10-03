30a67c835a70c0aab063474eeb6324a8d16636aa
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-03 16:08:24 -0500

    feat: add news fields and views

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,53 +2,31 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Constants\DocumentTypes;
 use App\Constants\Roles;
+use App\Domain\Auth\Action\StoreRegisteredUserAction;
 use App\Http\Controllers\Controller;
-use App\Infrastructure\Persistence\Models\User;
-use Illuminate\Auth\Events\Registered;
+use App\Http\Requests\User\StoreUserRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     */
     public function create(): Response
     {
-        return Inertia::render('Auth/Register');
+        $roleGuest = Roles::GUEST->value;
+        $documentTypes = DocumentTypes::getDocumentTypes();
+
+        return Inertia::render('Auth/Register', compact('documentTypes', 'roleGuest'));
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): RedirectResponse
+
+    public function store(StoreUserRequest $request, StoreRegisteredUserAction $action): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        $user->assignRole(Roles::GUEST);
-
-        event(new Registered($user));
-
-        Auth::login($user);
+        $action->execute($request->validated());
 
         return redirect(route('dashboard', absolute: false));
     }
+
 }
--- app/Infrastructure/Persistence/Models/User.php
+++ app/Infrastructure/Persistence/Models/User.php
@@ -19,32 +19,22 @@ class User extends Authenticatable
     use HasRoles;
     use LaravelPermissionToVueJS;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'surname',
+        'document_type',
+        'document',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+
     protected function casts(): array
     {
         return [
