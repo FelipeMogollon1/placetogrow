f4732a7fb2a544054056d6a763221eb24c3d3074
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-11 10:37:48 -0500

    feat: add subscription collects

--- app/Console/Commands/TransactionsConsult.php
+++ app/Console/Commands/TransactionsConsult.php
@@ -9,7 +9,9 @@ use App\Infrastructure\Persistence\Models\Payment;
 use App\Infrastructure\Persistence\Models\Subscription;
 use App\Jobs\Invoice\SolutionInvoiceJob;
 use App\Jobs\Payment\SolutionPaymentJob;
+use App\Jobs\Subscription\SolutionAutomaticSubscriptionPayment;
 use App\Jobs\Subscription\SolutionSubscriptionJob;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -43,6 +45,12 @@ class TransactionsConsult extends Command
         $subscriptions = Subscription::where('status', SubscriptionStatus::PENDING->value)->get();
         foreach ($subscriptions as $subscription) {
             SolutionSubscriptionJob::dispatch($subscription);
+        }
+
+        $subscriptionCollects = Subscription::where('status', SubscriptionStatus::ACTIVE->value)
+            ->whereDate('next_billing_date', Carbon::now()->toDateString())->get();
+        foreach ($subscriptionCollects as $subscriptionCollect) {
+            SolutionAutomaticSubscriptionPayment::dispatch($subscriptionCollect);
         }
 
         $invoices = Invoice::where('status', PaymentStatus::PENDING->value)->get();
