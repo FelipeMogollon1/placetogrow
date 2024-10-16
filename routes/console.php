2919c8b2a081045a05c570c0c5b220cdb67953ca
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-16 10:44:02 -0500

    feat: add jobs notify console

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Jobs\Notify\NotifyUserAboutInvoice;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:transactions-consult')
     ->withoutOverlapping(10)->everyFifteenMinutes()->runInBackground();
+
+Schedule::command('notify:expiring-invoices-subscriptions')
+    ->withoutOverlapping(10)->dailyAt('13:00')->runInBackground();
