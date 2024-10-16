8af59d36690bfe33837e0e5f14b2848c2279fa91
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-16 10:40:53 -0500

    feat: add notification invoices and subscriptions

---
+++ app/Notifications/InvoiceExpirationNotification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InvoiceExpirationNotification extends Notification
+{
+    use Queueable;
+
+    protected $invoice;
+
+    public function __construct($invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage())
+            ->subject('Alerta de Vencimiento de Factura')
+            ->greeting('Hola ' . $this->invoice->name)
+            ->line('Tu factura con referencia ' . $this->invoice->reference . ' está próxima a vencer el ' . $this->invoice->expiration_date->format('d/m/Y') . '.')
+            ->action('Ver Factura', url('/invoices/' . $this->invoice->id))
+            ->line('Gracias por confiar en nuestro servicio.');
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [
+            'invoice_id' => $this->invoice->id,
+            'expiration_date' => $this->invoice->expiration_date,
+        ];
+    }
+}
