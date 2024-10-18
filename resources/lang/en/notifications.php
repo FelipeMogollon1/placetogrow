4c0be13731d3acca4a012caa00d2173b162c9621
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-18 09:23:46 -0500

    feat: add news translations for notifications

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'subject' => 'Recordatorio de Pago de Factura',
+    'greeting' => 'Hola :name,',
+    'line1' => 'Queremos informarte que tu factura de :microsite con referencia :reference está próxima a vencer.',
+    'amount' => '**Monto a pagar**: :currency :amount',
+    'due_date' => '**Fecha de vencimiento**: :date',
+    'platform_link' => 'Para poder realizar el pago, por favor ingresa a nuestra plataforma visitando el siguiente enlace:',
+    'thanks' => 'Gracias por utilizar nuestros servicios. Si ya realizaste el pago, por favor ignora este mensaje.',
+    'pay_button' => 'Pagar Factura',
+];
---
+++ resources/lang/es/notifications.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'subject' => 'Invoice Payment Reminder',
+    'greeting' => 'Hello :name,',
+    'line1' => 'We would like to inform you that your invoice from :microsite with reference :reference is due soon.',
+    'amount' => '**Amount to Pay**: :currency :amount',
+    'due_date' => '**Due Date**: :date',
+    'platform_link' => 'To make the payment, please visit our platform using the following link:',
+    'thanks' => 'Thank you for using our services. If you have already made the payment, please ignore this message.',
+    'pay_button' => 'Pay Invoice',
+];
