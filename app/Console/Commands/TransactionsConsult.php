863e3d136f846eb4c6a071b221ba4dce933b4ec0
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-19 23:17:26 -0500

    feat: job submissions are separated into commands

--- app/Console/Commands/TransactionsConsult.php
+++ app/Console/Commands/TransactionsConsult.php
@@ -9,9 +9,7 @@ use App\Infrastructure\Persistence\Models\Payment;
 use App\Infrastructure\Persistence\Models\Subscription;
 use App\Jobs\Invoice\SolutionInvoiceJob;
 use App\Jobs\Payment\SolutionPaymentJob;
-use App\Jobs\Subscription\SolutionAutomaticSubscriptionPayment;
 use App\Jobs\Subscription\SolutionSubscriptionJob;
-use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -45,12 +43,6 @@ class TransactionsConsult extends Command
         $subscriptions = Subscription::where('status', SubscriptionStatus::PENDING->value)->get();
         foreach ($subscriptions as $subscription) {
             SolutionSubscriptionJob::dispatch($subscription);
-        }
-
-        $subscriptionCollects = Subscription::where('status', SubscriptionStatus::ACTIVE->value)
-            ->whereDate('next_billing_date', Carbon::now()->toDateString())->get();
-        foreach ($subscriptionCollects as $subscriptionCollect) {
-            SolutionAutomaticSubscriptionPayment::dispatch($subscriptionCollect);
         }
 
         $invoices = Invoice::where('status', PaymentStatus::PENDING->value)->get();
--- app/Console/Commands/UpcomingSubscriptionCollectionsCommand.php
+++ app/Console/Commands/UpcomingSubscriptionCollectionsCommand.php
@@ -2,54 +2,42 @@
 
 namespace App\Console\Commands;
 
-use App\Constants\PaymentStatus;
 use App\Constants\SubscriptionPeriods;
 use App\Constants\SubscriptionStatus;
-use App\Infrastructure\Persistence\Models\Invoice;
 use App\Infrastructure\Persistence\Models\Subscription;
-use App\Jobs\Notify\NotifyUserAboutInvoice;
 use App\Jobs\Notify\NotifyUserAboutSubscription;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
-class ExpiringInvoicesSubscriptions extends Command
+class UpcomingSubscriptionCollectionsCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:Expiring-invoices-subscriptions';
+    protected $signature = 'app:upcoming-subscription-collections';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Notifies users about invoices and subscriptions that are expiring in 5 days';
+    protected $description = 'Notifies users about upcoming subscription collections';
 
-    /**
-     * Execute the console command.
-     */
     public function handle(): void
     {
-        Log::info('start the command app:Expiring-invoices-subscriptions');
+        Log::info('Starting command: app:upcoming-subscription-collections');
 
-        $invoicesNotify = Invoice::where('status', PaymentStatus::PENDING->value)
-            ->whereDate('expiration_date', now()->addDays(5))->get();
-
-        foreach ($invoicesNotify as $invoice) {
-            NotifyUserAboutInvoice::dispatch($invoice);
-        }
-
+        $daysBeforeSubscriptionCollection = (int) config('notifications.days_before_subscription_collection');
 
         $subscriptionsNotify = Subscription::join('subscription_plans', 'subscriptions.subscription_plan_id', '=', 'subscription_plans.id')
-            ->select('subscriptions.*')
+            ->select('subscriptions.id, subscriptions.email, subscriptions.name, subscriptions.surname, subscriptions.reference, subscriptions.total_charges, subscription_plans.expiration_time')
             ->where('subscription_plans.subscription_period', '<>', SubscriptionPeriods::DAILY->value)
             ->whereColumn('subscriptions.total_charges', '<', 'subscription_plans.expiration_time')
             ->where('subscriptions.status', SubscriptionStatus::ACTIVE->value)
-            ->whereDate('next_billing_date', Carbon::now()->addDays(5)->toDateString())
+            ->whereBetween('next_billing_date', [Carbon::now(), Carbon::now()->addDays($daysBeforeSubscriptionCollection)])
             ->get();
 
         foreach ($subscriptionsNotify as $subscriptionNotify) {
@@ -57,7 +45,6 @@ class ExpiringInvoicesSubscriptions extends Command
             $this->info('Notification dispatched for subscription: ' . $subscriptionNotify->reference);
         }
 
-        Log::info('finish the command app:Expiring-invoices-subscriptions');
-
+        Log::info('Finished command: app:upcoming-subscription-collections');
     }
 }
