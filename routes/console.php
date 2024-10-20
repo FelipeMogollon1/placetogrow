f5c9e011566c659109eeff780d1422da74cd1b54
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-10-19 23:22:53 -0500

    feat: add new individual schedules

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,15 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('app:transactions-consult')
     ->withoutOverlapping(10)->everyFifteenMinutes()->runInBackground();
 
-Schedule::command('app:expiring-invoices-subscriptions')
+Schedule::command('app:daily-subscription-collection')
     ->withoutOverlapping(10)->dailyAt('10:00')->runInBackground();
+
+Schedule::command('app:expiring-invoices')
+    ->withoutOverlapping(10)->dailyAt('11:00')->runInBackground();
+
+Schedule::command('app:expiring-subscriptions')
+    ->withoutOverlapping(10)->dailyAt('12:00')->runInBackground();
+
+Schedule::command('app:upcoming-subscription-collections')
+    ->withoutOverlapping(10)->dailyAt('13:00')->runInBackground();
+
